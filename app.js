2c57b8f0e584ba57eb4f73a974df6b71b119c8cf
jpsumo <jaredmpatch@gmail.com>
2020-02-01 08:15:07 -0600

    adjust css to allow for all colors

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 function randomRGB() {
     const r = Math.floor(Math.random() * 256);
-    const g = 0;
+    const g = Math.floor(Math.random() * 256);
     const b = Math.floor(Math.random() * 256);
     return `rgb(${r}, ${g}, ${b})`
 }
@@ -11,6 +11,6 @@ setInterval(function(){
     for(let letter of letters){
         letter.style.color = randomRGB();
     }
-}, 800);
+}, 1000);
 
 
